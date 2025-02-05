7e16a699fb0be1a36de928d23dbef6ae66d7cc0c
EnergoStalin <ens.stalin@gmail.com>
2025-02-05 20:22:48 +0300

    fix(ci): build version generator

---
+++ increment.mjs
@@ -0,0 +1,24 @@
+import { readFileSync } from "node:fs"
+
+/**
+ * @type { {version: string} }
+ */
+// @ts-ignore
+const pkg = JSON.parse(
+	readFileSync("package.json", { encoding: "utf8", flag: "r" }),
+)
+
+const currentProductVersion = process.env.VERSION
+if (!currentProductVersion) throw new Error('No product version specified')
+
+const [version, buildVersion] = pkg.version.split('+')
+if (!version || !buildVersion) throw new Error('Version parsing failed')
+
+const productVersion = version.slice(0, version.lastIndexOf(buildVersion))
+
+let buildNumber = parseInt(buildVersion, 10) + 1
+if (productVersion !== currentProductVersion) {
+	buildNumber = 1
+}
+
+console.log(`${process.env.VERSION}${buildNumber}+${buildNumber}`)
