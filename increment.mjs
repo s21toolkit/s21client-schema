9726e2f3f27bca7f070fd2411584b0354f1e39e2
EnergoStalin <ens.stalin@gmail.com>
2025-02-06 20:19:21 +0300

    fix(ci): move from npm version to updating package.json manually along with creating git tag etc...

--- increment.mjs
+++ increment.mjs
@@ -1,4 +1,23 @@
-import { readFileSync } from "node:fs"
+import { readFileSync, writeFileSync } from "node:fs"
+
+/**
+ * @param {string} currentVersion
+ * @param {number} currentBuildVersion
+ * @param {{ (p: number, b: number): number }} cb
+ */
+function getVersionWithBuildNumber(currentVersion, currentBuildVersion, cb) {
+	const [major, minor, patch] = currentVersion
+		.split(".")
+		.map((e) => Number.parseInt(e, 10))
+	if (
+		Number.isNaN(major) ||
+		Number.isNaN(minor) ||
+		Number.isNaN(patch) ||
+		patch === undefined
+	)
+		throw new Error("Semver parsing failed")
+	return `${major}.${minor}.${Math.max(cb(patch, currentBuildVersion), 0)}`
+}
 
 /**
  * @type { {version: string} }
@@ -8,17 +27,37 @@ const pkg = JSON.parse(
 	readFileSync("package.json", { encoding: "utf8", flag: "r" }),
 )
 
-const currentProductVersion = process.env.VERSION
-if (!currentProductVersion) throw new Error("No product version specified")
+const newProductVersion = process.env.VERSION
+if (!newProductVersion) throw new Error("No product version specified")
 
-const [version, buildVersion] = pkg.version.split("+")
-if (!version || !buildVersion) throw new Error("Version parsing failed")
+const [currentVersion, currentBuildVersion] = pkg.version.split("+")
+if (!currentVersion || !currentBuildVersion)
+	throw new Error("Version parsing failed")
 
-const productVersion = version.slice(0, version.lastIndexOf(buildVersion))
+const currentBuildNumber = Number.parseInt(currentBuildVersion, 10)
+const currentProductVersion = getVersionWithBuildNumber(
+	currentVersion,
+	currentBuildNumber,
+	(p, b) => p - b,
+)
 
-let buildNumber = Number.parseInt(buildVersion, 10) + 1
-if (productVersion !== currentProductVersion) {
-	buildNumber = 1
+let newBuildVersion = currentBuildNumber + 1
+if (
+	currentProductVersion.split(".").slice(0, 2).join(".") !==
+	newProductVersion.split(".").slice(0, 2).join(".")
+) {
+	newBuildVersion = 1
 }
 
-console.log(`${currentProductVersion}${buildNumber}+${buildNumber}`)
+// console.log(`${currentBuildVersion} => ${newBuildVersion}`, `${currentProductVersion} => ${newProductVersion}`)
+const newVersion = getVersionWithBuildNumber(
+	newProductVersion,
+	newBuildVersion,
+	(p, b) => p + b,
+)
+pkg.version = `${newVersion}+${newBuildVersion}`
+writeFileSync("package.json", JSON.stringify(pkg, undefined, 2), {
+	encoding: "utf8",
+})
+
+console.log(pkg.version)
